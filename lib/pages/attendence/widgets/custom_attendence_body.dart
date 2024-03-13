ae2b026753e7e630172c4a1c4bc111e56d777eec
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-03-13 13:18:48 +0200

    finish attendence page

---
+++ lib/pages/attendence/widgets/custom_attendence_body.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:flutter/material.dart';
+
+import '../../../utlities/widgets/custom_appbar.dart';
+import 'custom_attendence_main_container.dart';
+
+
+class CustomAttendenceBody extends StatelessWidget {
+  const CustomAttendenceBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        CustomAppbarMain(text: 'Records', icon: Icons.arrow_back_ios,),
+        CustomAttendenceMainContainer(),
+
+      ],
+    );
+  }
+}
