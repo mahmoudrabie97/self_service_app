4a2e8b595414f46d0f14a099c9ba8d0d5b82deee
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-03-19 15:02:40 +0200

    add request cubit

--- lib/network/endpoints.dart
+++ lib/network/endpoints.dart
@@ -1,4 +1,7 @@
 class ApiEndPoint {
   static const String baseurl = 'http://hr_app.erpmakeen.com:8079/';
   static const String loginUrl = 'web/session/authenticate';
+
+  static const String listTimeRequestUrl = 'list_time_off_requests';
 }
+//list_time_off_requests
