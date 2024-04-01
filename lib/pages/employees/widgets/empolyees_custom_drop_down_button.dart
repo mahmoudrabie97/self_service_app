3f0a8c52cdcca258884069541f2aa74f89bd6143
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-04-01 10:29:03 +0200

    edit types of requests

--- lib/pages/employees/widgets/empolyees_custom_drop_down_button.dart
+++ lib/pages/employees/widgets/empolyees_custom_drop_down_button.dart
@@ -25,15 +25,21 @@ class _CustomDropDownButtonState extends State<EmployeesCustomDropDownButton> {
         isExpanded: true,
         value: widget.dropDownValue,
 
-        icon: Icon(
+        icon: const Icon(
           Icons.keyboard_arrow_down,
           //size: 25,
+
           color: Colors.brown,
         ),
         onChanged: (String? newValue) {
-          setState(() {
+          setState(()
+          {
+
             widget.dropDownValue = newValue!;
-          });
+
+          }
+          );
+
         },
         items: const [
           DropdownMenuItem(
--- lib/pages/types_of_request/widgets/types_of_requests_body.dart
+++ lib/pages/types_of_request/widgets/types_of_requests_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../utlities/widgets/custom_appbar.dart';
+import '../../../utlities/widgets/custom_personal_apbar.dart';
 import 'custom_types_requests_main_container.dart';
 
 
@@ -10,9 +11,12 @@ class CustomTypesOfRequestBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children:  [
-        CustomAppbarMain(text: 'Types of requests', icon: Icons.arrow_back_ios,),
-        CustomTypesRequestMainContainer(),
+      children:   [
+        Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: CustomPersonalApbar(  title: 'Types of requests', subTitle: '',),
+        ),
+        const CustomTypesRequestMainContainer(),
       ],
     );
   }
